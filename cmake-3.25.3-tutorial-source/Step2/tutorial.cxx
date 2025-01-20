1f4905e00d0d503f114630f04fe5b5d2ca70900b
Percy Cho <percycho@outlook.com>
2025-01-20 18:54:27 +0800

    mod: cmake tutorial

--- cmake-3.25.3-tutorial-source/Step2/tutorial.cxx
+++ cmake-3.25.3-tutorial-source/Step2/tutorial.cxx
@@ -5,9 +5,9 @@
 
 #include "TutorialConfig.h"
 
-// TODO 11: Only include MathFunctions if USE_MYMATH is defined
-
-// TODO 5: Include MathFunctions.h
+#ifdef USE_MYMATH
+  #include "MathFunctions.h"
+#endif
 
 int main(int argc, char* argv[])
 {
@@ -22,12 +22,12 @@ int main(int argc, char* argv[])
   // convert input to double
   const double inputValue = std::stod(argv[1]);
 
-  // TODO 12: Use mysqrt if USE_MYMATH is defined and sqrt otherwise
-
-  // TODO 6: Replace sqrt with mysqrt
-
   // calculate square root
+#ifdef USE_MYMATH
+  const double outputValue = mysqrt(inputValue);
+#else
   const double outputValue = sqrt(inputValue);
+#endif
   std::cout << "The square root of " << inputValue << " is " << outputValue
             << std::endl;
   return 0;
